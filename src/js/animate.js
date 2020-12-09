c98644830eaf199638c8d75427f1219bc2914143
Pasha Grinchenko <kvartalshow@gmail.com>
2020-12-09 16:08:28 +0200

    Add block gallery, add adaptive too :tada:

--- src/js/animate.js
+++ src/js/animate.js
@@ -6,16 +6,16 @@ AOS.init({
     animatedClassName: 'aos-animate', // class applied on animation
     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
     disableMutationObserver: true, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    debounceDelay: 0, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 0, // the delay on throttle used while scrolling the page (advanced)
     
   
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 100, // offset (in px) from the original trigger point
-    delay: 50, // values from 0 to 3000, with step 50ms
-    duration: 600, // values from 0 to 3000, with step 50ms
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 500, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   
